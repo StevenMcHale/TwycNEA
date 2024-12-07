257f08587bc5f183526b1062e3446e1edfb2572d
StevenMcHale <steven.mchale@icloud.com>
2024-12-07 00:26:40 +0000

    200/150

--- auto/extras.py
+++ auto/extras.py
@@ -1,7 +1,7 @@
 class DynamicQueue(object):
     def __init__(self):
         self.__items = []
-        self.__max = 100
+        self.__max = 200
 
     def enQueue(self, item):
         self.__items.append(item)
