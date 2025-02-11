feabed212ff1c7e20615bfdcd01344bf674a4ef2
StevenMcHale <steven.mchale@icloud.com>
2025-02-11 17:04:49 +0000

    include map to prove precess speeds

--- auto/views.py
+++ auto/views.py
@@ -156,7 +156,7 @@ def automatic(request):
 
                         # Calculate Breaks and Distance                           
 
-                        distance = 10
+                        distance = 0
                         breaks = 0
                         teachersOrder = []
 
@@ -174,23 +174,23 @@ def automatic(request):
                         # Put through graph
                         
 
-                        # count = 0
+                        count = 0
 
-                        # while count < len(teachersOrder)-1:
+                        while count < len(teachersOrder)-1:
 
-                        #     currentTeacher = Teacher.objects.get(name=teachersOrder[count])
-                        #     nextTeacher = Teacher.objects.get(name=teachersOrder[count+1])
+                            currentTeacher = Teacher.objects.get(name=teachersOrder[count])
+                            nextTeacher = Teacher.objects.get(name=teachersOrder[count+1])
 
-                        #     map = twyc_map
-                        #     currentNode = map.getNode(currentTeacher.building.name)
+                            map = twyc_map
+                            currentNode = map.getNode(currentTeacher.building.name)
 
-                        #     if currentTeacher.building != nextTeacher.building:
-                        #         nodeWeight = currentNode.getLinkWeight(nextTeacher.building.name)
-                        #     else:
-                        #         nodeWeight = 0
+                            if currentTeacher.building != nextTeacher.building:
+                                nodeWeight = currentNode.getLinkWeight(nextTeacher.building.name)
+                            else:
+                                nodeWeight = 0
 
-                        #     distance += nodeWeight
-                        #     count += 1
+                            distance += nodeWeight
+                            count += 1
 
                         currentDict['Breaks'] = breaks
                         currentDict['Distance'] = distance
